ddb4308e4ce46996f365d823949bd406675579f2
Kalle Hallbäck <kalle.hallback@informer.se>
2026-09-01 13:27:29 +0200

    yes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import BoardPage from "./pages/BoardPage";
 
 export default function App() {
   return (
-    <div>
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/boards/:id" element={<BoardPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
