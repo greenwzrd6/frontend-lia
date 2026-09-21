24993d6edf041b9d35f4fd9332c337ef9ec53d3a
David Lindberg <david.lindberg@informer.se>
2026-09-21 09:32:14 +0200

    letsagooooo

--- src/hooks/useBoard.ts
+++ src/hooks/useBoard.ts
@@ -2,12 +2,13 @@ import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
 
 import { getBoard } from "../services/boardApi";
+import { boardKeys } from "../utils/queryKeys";
 
 export function useBoard() {
   const { id } = useParams<{ id: string }>();
 
   return useQuery({
-    queryKey: ["board", id],
+    queryKey: boardKeys.byId(id),
     queryFn: () => getBoard(id!),
     enabled: !!id,
   });
