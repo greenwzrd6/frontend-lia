aae4824c3055a7ceef3f97ec69e30c07b70ebd6a
Kalle Hallbäck <kalle.hallback@informer.se>
2026-09-21 09:47:05 +0200

    implemented mock nodes and connections for testing

--- src/components/Column/ColumnEdgeCreator.tsx
+++ src/components/Column/ColumnEdgeCreator.tsx
@@ -1,13 +1,80 @@
-import { ReactFlow, Background, Controls } from "@xyflow/react";
+import {
+  ReactFlow,
+  Background,
+  Controls,
+  applyNodeChanges,
+  applyEdgeChanges,
+  type Node,
+  type Edge,
+  addEdge,
+} from "@xyflow/react";
 import "@xyflow/react/dist/style.css";
+import { useCallback, useState } from "react";
+
+type ColumnNode = Node<{ label: string }>;
+
+const initialNodes: ColumnNode[] = [
+  {
+    id: "n1",
+    position: { x: 0, y: 0 },
+    data: { label: "Node 1" },
+    type: "input",
+  },
+  {
+    id: "n2",
+    position: { x: 100, y: 100 },
+    data: { label: "Node 2" },
+    type: "output",
+  },
+  {
+    id: "n3",
+    position: { x: 200, y: 200 },
+    data: { label: "Node 3" },
+    type: "output",
+  },
+];
+
+const initialEdges: Edge[] = [
+  {
+    id: "n1-n2",
+    source: "n1",
+    target: "n2",
+  },
+];
 
 export default function ColumnEdgeCreator() {
+  const [nodes, setNodes] = useState(initialNodes);
+  const [edges, setEdges] = useState(initialEdges);
+
+  const onNodesChange = useCallback(
+    (changes: any) =>
+      setNodes((nodesSnapshot) => applyNodeChanges(changes, nodesSnapshot)),
+    [],
+  );
+  const onEdgesChange = useCallback(
+    (changes: any) =>
+      setEdges((edgesSnapshot) => applyEdgeChanges(changes, edgesSnapshot)),
+    [],
+  );
+  const onConnect = useCallback(
+    (params: any) =>
+      setEdges((edgesSnapshot) => addEdge(params, edgesSnapshot)),
+    [],
+  );
+
   return (
     <div
       className="border rounded-2xl bg-white"
       style={{ height: "80%", width: "80%" }}
     >
-      <ReactFlow>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        fitView
+      >
         <Background />
         <Controls />
       </ReactFlow>
