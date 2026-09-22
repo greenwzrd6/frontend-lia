5d7bf9d21aeb11c75bee4aba74446b775b4de2ed
Kalle Hallbäck <kalle.hallback@informer.se>
2026-09-22 10:02:22 +0200

    come on do math

--- src/components/Column/Flow/BoardGroup.tsx
+++ src/components/Column/Flow/BoardGroup.tsx
@@ -2,7 +2,7 @@ import type { NodeProps } from "@xyflow/react";
 
 export default function BoardGroup({ data }: NodeProps) {
   return (
-    <div className="size-full rounded-lg border-2 border-gray-400">
+    <div className="size-full rounded-lg border-2 border-gray-400 cursor-default">
       <div className="absolute -top-7 text-lg">{String(data.label)}</div>
     </div>
   );
